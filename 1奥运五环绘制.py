bbe238301a9fca2ecde5b975b33a250c89714b5e
sunnsunny <sunnunicorn@163.com>
2024-04-22 18:29:17 +0800

    aaa
    aaa

---
+++ 1奥运五环绘制.py
@@ -0,0 +1,58 @@
+import turtle   #导入海龟绘图
+
+# # 显示箭头
+# turtle.showturtle()
+# #显示一行字
+# turtle.write("sunn 你好，我是小海龟")
+# #前进300
+# turtle.forward(300)
+# #画笔换成红色
+# turtle.color("red")
+# #画笔左转90°
+# turtle.left(90)
+# turtle.forward(300)
+# #去坐标(0, 50)
+# turtle.goto(0, 50)
+# turtle.goto(0, 0)
+# turtle.penup()
+# turtle.goto(0, 300)
+# turtle.pendown()
+# #画圆 半径为100
+# turtle.circle(100)
+
+turtle.penup()
+turtle.goto(-210, 200)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(50)
+# turtle.write("200,200")
+
+turtle.penup()
+turtle.goto(-130, 200)
+turtle.pendown()
+turtle.circle(50)
+# turtle.write("130,200")
+
+turtle.penup()
+turtle.goto(-50, 200)
+turtle.pendown()
+turtle.circle(50)
+# turtle.write("50,200")
+
+
+turtle.penup()
+turtle.goto(-170, 150)
+turtle.pendown()
+turtle.circle(50)
+
+turtle.penup()
+turtle.goto(-90, 150)
+turtle.pendown()
+turtle.circle(50)
+
+
+
+
+#程序结束，保持在窗口存在
+turtle.done()
+
---
+++ object.py
@@ -0,0 +1,10 @@
+
+
+
+a = 3
+print(id(a))
+print(type(a))
+
+b = "Hello Sunn"
+print(id(b))
+print(type(b))
